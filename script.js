d38af4c21de20e624a4f237a8febc601c1f09768
Franck kapuya <franckkapuya13@gmail.com>
2024-05-28 16:57:53 +0100

    step two to three

--- script.js
+++ script.js
@@ -11,26 +11,26 @@ var tl = gsap.timeline({
 
 // Animation de l'élément "#fanta"
 tl.to("#fanta", {
-    top: "120%",
+    top: "115%",
     left: "0%"
 }, 'orange')
 
 // Animation de l'élément "#orange-cut"
 tl.to("#orange-cut", {
-    top:"160%",
+    top:"150%",
     left: "23%"
 }, 'orange')
 
 // Animation de l'élément "#orange"
 tl.to("#orange", {
     width: "15%",
-    top:"160%",
+    top:"170%",
     right: "10%"
 }, 'orange')
 
 // Animation de l'élément "#leaf"
 tl.to("#leaf", {
-    top:"110%",
+    top:"100%",
     rotate: "130deg",
     left: "70%"
 }, 'orange')
@@ -41,3 +41,26 @@ tl.to("#leaf2", {
     rotate: "130deg",
     left: "0%"
 }, 'orange')
+
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".three",         // L'élément déclencheur de l'animation
+        start: "0% 95%",         // Le début de l'animation (0% de la hauteur de la fenêtre)
+        end: "70% 50%",          // La fin de l'animation (70% de la hauteur de la fenêtre)
+        scrub: true,             // Active le "scrub" (défilement dans les deux sens)
+        markers: true,        // Active les marqueurs de débogage (optionnel)
+    }
+})
+
+// tl2.from(".lemon1", {
+//     rotate: "-90deg",
+//     left : "-25%",
+//     top:"110%"
+// }, 'ca')
+
+// tl2.from("#coca", {
+//     rotate: "-95deg",
+//     // left : "-30%",
+//     top:"50%"
+// }, 'ca')
